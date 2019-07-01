a0ad735aa01640d948ab7aa5c5ae97bd4da108fe
Dhrubanka <dhrubankachutia@gmail.com>
2019-07-01 18:40:01 +0530

    init

---
+++ app/src/main/java/info/bankaspace/servicecreators/VolleySingleton.java
@@ -0,0 +1,40 @@
+package info.bankaspace.servicecreators;
+
+
+import android.content.Context;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+/**
+ * Created by DC on 25-02-2018.
+ */
+
+public class VolleySingleton {
+    private static VolleySingleton mInstance;
+    private RequestQueue mRequestQueue;
+    private static Context mCtx;
+
+    private VolleySingleton(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+    }
+
+    public static synchronized VolleySingleton getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new VolleySingleton(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+}
